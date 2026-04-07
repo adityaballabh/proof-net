475d7bc39d4a6dd7da5294705bcae571f28f7b93
Aditya Ballabh <26791872+adityaballabh@users.noreply.github.com>
2026-04-07 10:07:49 -0500

    add onion routing

--- src/proof_net.cpp
+++ src/proof_net.cpp
@@ -9,6 +9,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <signal.h>
@@ -22,22 +23,25 @@ struct Node{
     string ip;
 };
 
-struct Packet{
+struct Message{
     string id, content;
     deque<int> route;
-    int prev_node, size;
+};
+
+struct Packet{
+    string id, payload;
 };
 
 struct Receipt{
     string packet_id, signature;
-    int generator, bytes;
+    int generator, receiver, bytes;
 };
 
 struct PubKey{
-    unsigned char val[crypto_sign_ed25519_PUBLICKEYBYTES];
+    unsigned char signing[crypto_sign_ed25519_PUBLICKEYBYTES], encryption[crypto_box_PUBLICKEYBYTES];
 };
 
-const int BACKLOG = 8, MAX_LEN = 1024;
+const int BACKLOG = 8, MAX_LEN = 4096;
 const string receipt_prefix = "receipt ";
 
 unordered_map<int, Node> getConfig(string config_path){
@@ -123,16 +127,6 @@ int createConnection(string ip, int port){
     return sockfd;
 }
 
-string convertPacket(Packet packet){
-    deque<int> route = packet.route;
-    string msg = packet.id + ' ' + to_string(packet.prev_node) + ' ' + to_string(route[0]);
-    int hops = route.size();
-    for(int i = 1; i < hops; i++)
-        msg += ',' + to_string(packet.route[i]);
-    msg += ' ' + packet.content + '\n';
-    return msg;
-}
-
 void sendWrapper(string message, int sockfd){
     int len = message.size(), sent = 0;
     while(sent < len){
@@ -145,86 +139,129 @@ void sendWrapper(string message, int sockfd){
     }
 }
 
-string getPayload(Receipt receipt){
-    return receipt.packet_id + ' ' + to_string(receipt.generator) + ' ' + to_string(receipt.bytes);
+string getReceiptPayload(Receipt receipt){
+    return receipt.packet_id + ' ' + to_string(receipt.generator) + ' ' + to_string(receipt.receiver) + ' ' + to_string(receipt.bytes);
 }
 
 string convertReceipt(Receipt receipt){
-    return receipt_prefix + ' ' + getPayload(receipt) + ' ' + receipt.signature  + '\n';
+    return receipt_prefix + ' ' + getReceiptPayload(receipt) + ' ' + receipt.signature  + '\n';
 }
 
 Receipt parseReceipt(string receipt_str){
     stringstream ss(receipt_str);
     string pref;
     Receipt receipt;
-    ss >> pref >> receipt.packet_id >> receipt.generator >> receipt.bytes >> receipt.signature;
+    ss >> pref >> receipt.packet_id >> receipt.generator >> receipt.receiver >> receipt.bytes >> receipt.signature;
     return receipt;
 }
 
 void storeReceipt(Receipt receipt){
     string file_path = "receipts/" + receipt.packet_id + ".txt";
     ofstream out(file_path);
-    out << getPayload(receipt) + ' ' + receipt.signature << '\n';
+    out << getReceiptPayload(receipt) + ' ' + receipt.signature << '\n';
+}
+
+string getBase64Encoded(unsigned char* data, int len){
+    int b64_len = sodium_base64_encoded_len(len, sodium_base64_VARIANT_ORIGINAL);
+    char b64_encoded[b64_len];
+    return sodium_bin2base64(b64_encoded, b64_len, data, len, sodium_base64_VARIANT_ORIGINAL);                             
 }
 
 void signReceipt(Receipt &receipt, unsigned char *pvt_key){
-    string payload = getPayload(receipt);
+    string payload = getReceiptPayload(receipt);
     unsigned char signature[crypto_sign_BYTES];
 
-    int b64_len = sodium_base64_encoded_len(crypto_sign_BYTES, sodium_base64_VARIANT_ORIGINAL);
-    char b64_signature[b64_len];
-
-    crypto_sign_detached(signature, NULL, (unsigned char*) payload.c_str(), payload.size(), pvt_key);                                                                                                                                 
-    receipt.signature = sodium_bin2base64(b64_signature, sizeof(b64_signature), signature, crypto_sign_BYTES, sodium_base64_VARIANT_ORIGINAL);                             
+    crypto_sign_detached(signature, NULL, (unsigned char*) payload.c_str(), payload.size(), pvt_key);
+    receipt.signature = getBase64Encoded(signature, crypto_sign_BYTES);
 }
 
 bool isValidReceipt(Receipt receipt, PubKey pub_key){
-    string payload = getPayload(receipt);
+    string payload = getReceiptPayload(receipt);
     unsigned char signature[crypto_sign_BYTES];
 
     if(sodium_base642bin(signature, sizeof(signature), receipt.signature.c_str(), receipt.signature.size(), NULL, NULL, NULL, sodium_base64_VARIANT_ORIGINAL))
         return false;
 
-    bool valid = !crypto_sign_verify_detached(signature, (unsigned char*) payload.c_str(), payload.size(), pub_key.val);
+    bool valid = !crypto_sign_verify_detached(signature, (unsigned char*) payload.c_str(), payload.size(), pub_key.signing);
     return valid;
 }
 
-void processPacket(unordered_map<int, Node> &config, unordered_map<int, PubKey> &pub_keys, unsigned char* pvt_key, Packet packet, int node_id){
-    deque<int> route = packet.route;
-    if(route.empty() || route.front() != node_id)
-        throw runtime_error("incorrectly routed packet");
-    
-    if(packet.prev_node != -1){
-        Node prev_hop = config[packet.prev_node];
+string getEncrypted(unsigned char *pub_key, string message){
+    string encrypted;
+    int encrypted_len = message.size() + crypto_box_SEALBYTES;
+    encrypted.resize(encrypted_len);
+    crypto_box_seal((unsigned char*) encrypted.data(), (unsigned char*) message.data(), message.size(), pub_key);
+    return encrypted;
+}
+
+string getOnionEncrypted(unordered_map<int, PubKey> &pub_keys, deque<int> route, string content){
+    // next_hop for dest is -1
+    int next_hop = -1, n = route.size(), node = route[n - 1];
+    string curr = string((char*) &next_hop, 4) + content, encrypted;
+    encrypted = getEncrypted(pub_keys[node].encryption, curr);
+
+    for(int i = n - 2; i >= 0; i--){
+        next_hop = htonl(route[i + 1]), node = route[i];
+        curr = string((char*) &next_hop, 4) + encrypted;
+        encrypted = getEncrypted(pub_keys[node].encryption, curr);
+    }
+    return getBase64Encoded((unsigned char*) encrypted.data(), encrypted.size());
+}
+
+pair<int, string> getOnionDecrypted(PubKey &node_pub, unsigned char *pvt_encryption, string encoded){
+    string bin, decoded, payload;
+    bin.resize(encoded.size());
+    size_t bin_len;
+    if(sodium_base642bin((unsigned char*) bin.data(), bin.size(), encoded.c_str(), encoded.size(), NULL, &bin_len, NULL, sodium_base64_VARIANT_ORIGINAL))
+        return {INT_MIN, ""};
+    bin.resize(bin_len);
+
+    decoded.resize(bin_len - crypto_box_SEALBYTES);
+    if(crypto_box_seal_open((unsigned char*) decoded.data(), (unsigned char*) bin.data(), bin_len, node_pub.encryption, pvt_encryption))
+        return {INT_MIN, ""};
+
+    int next_hop;
+    memcpy(&next_hop, decoded.data(), 4);
+    next_hop = ntohl(next_hop);
+    payload = decoded.substr(4);
+    return {next_hop, payload};
+}
+
+void processPacket(unordered_map<int, Node> &config, unordered_map<int, PubKey> &pub_keys, Packet packet, unsigned char* pvt_signing, unsigned char* pvt_encryption, int node_id, int prev_node){
+    if(prev_node != -1){
+        Node prev_hop = config[prev_node];
         int sockfd = createConnection(prev_hop.ip, prev_hop.port);
-        Receipt receipt{packet.id, "", node_id, packet.size};
-        signReceipt(receipt, pvt_key);
+        Receipt receipt{packet.id, "", node_id, prev_node, (int) packet.payload.size()};
+        signReceipt(receipt, pvt_signing);
         string receipt_str = convertReceipt(receipt);
         sendWrapper(receipt_str, sockfd);
         close(sockfd);
     }
 
-    route.pop_front();
-    if(route.empty()){
-        cout << node_id << " packet reached destination: " << packet.id << ' ' << packet.content << '\n';
+    auto [next_hop_id, payload] = getOnionDecrypted(pub_keys[node_id], pvt_encryption, packet.payload);
+    if(next_hop_id == INT_MIN){
+        cerr << node_id << " decryption failed for packet " + packet.id << '\n';
         return;
     }
-    int next_hop_id = route.front();
+    else if(next_hop_id == -1){
+        cout << node_id << " packet reached destination: " + packet.id + ' ' + payload << '\n';
+        return;
+    }
+    
     if(!config.count(next_hop_id))
         throw runtime_error("invalid next hop");
     
+    string b64_payload = getBase64Encoded((unsigned char*) payload.data(), payload.size());
     Node next_hop = config[next_hop_id];
     int sockfd = createConnection(next_hop.ip, next_hop.port);
     
-    packet.route = route;
-    packet.prev_node = node_id;
-    string message = convertPacket(packet);
+    string message = packet.id + ' ' + b64_payload + '\n';
     sendWrapper(message, sockfd);
     close(sockfd);
 }
 
 // messages are always a single line
-string getMessage(int sockfd){
+string getPacket(int sockfd){
     string data;
     char buf[MAX_LEN];
     size_t term_pos;
@@ -240,38 +277,68 @@ string getMessage(int sockfd){
     return data;
 }
 
-Packet parseMessage(string message){
+Message parseMessage(string message){
     stringstream ss_msg(message);
-    Packet packet;
+    Message msg;
     string route, tok, content;
-    ss_msg >> packet.id >> packet.prev_node >> route;
+    ss_msg >> msg.id >> route;
     getline(ss_msg, content);
     if(content.empty())
         throw runtime_error("no packet content found");
-    packet.content = content.substr(1);
+    msg.content = content.substr(1);
 
     stringstream ss_route(route);
     while(getline(ss_route, tok, ',')){
         int node = stoi(tok);
-        packet.route.push_back(node);
+        msg.route.push_back(node);
     }
-    packet.size = message.size();
+    return msg;
+}
+
+Packet parsePacket(string message){
+    stringstream ss(message);
+    Packet packet;
+    ss >> packet.id >> packet.payload;
     return packet;
 }
 
-vector<Packet> loadMessages(int node_id){
+vector<Packet> loadMessages(unordered_map<int, PubKey> &pub_keys, int node_id){
     vector<Packet> packets;
     string path = "messages/" + to_string(node_id) + ".txt", line;
     ifstream fp(path);
     while(getline(fp, line)){
         cout << node_id << " sending: " << line << '\n';
-        Packet packet = parseMessage(line);
+        Message message = parseMessage(line);
+        Packet packet;
+        packet.id = message.id;
+        packet.payload = getOnionEncrypted(pub_keys, message.route, message.content);
         packets.push_back(packet);
     }
     return packets;
 }
 
-void processConnections(unordered_map<int, Node> &config, unordered_map<int, PubKey> &pub_keys, unsigned char* pvt_key, int sockfd, int node_id){
+int getNodeID(unordered_map<int, Node> &config, sockaddr_storage addr){
+    addrinfo hints;
+    memset(&hints, 0, sizeof(hints));   
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    
+    in_addr_t ip_addr = ((sockaddr_in*) &addr)->sin_addr.s_addr;
+    for(auto [id, node] : config){
+        addrinfo *servinfo;
+        if(getaddrinfo(node.ip.c_str(), NULL, &hints, &servinfo) != 0)
+            continue;
+        sockaddr_in* curr = (sockaddr_in*) servinfo->ai_addr;
+        in_addr_t curr_addr = curr->sin_addr.s_addr;
+        freeaddrinfo(servinfo);
+
+        if(curr_addr == ip_addr)
+            return id;
+    }
+    return -1;
+}
+
+void processConnections(unordered_map<int, Node> &config, unordered_map<int, PubKey> &pub_keys, unsigned char* pvt_signing, unsigned char* pvt_encryption, int sockfd, int node_id){
     struct sigaction sa;
     sa.sa_handler = sigchld_handler;
     sigemptyset(&sa.sa_mask);
@@ -292,17 +359,18 @@ void processConnections(unordered_map<int, Node> &config, unordered_map<int, Pub
         }
         if(!fork()){
             close(sockfd);
-            string message = getMessage(new_fd);
-            cout << node_id << " received: " << message << '\n';
+            string packet_str = getPacket(new_fd);
+            cout << node_id << " received: " << packet_str << '\n';
             
-            if(message.substr(0, receipt_prefix.size()) == receipt_prefix){
-                Receipt receipt = parseReceipt(message);
+            if(packet_str.substr(0, receipt_prefix.size()) == receipt_prefix){
+                Receipt receipt = parseReceipt(packet_str);
                 if(isValidReceipt(receipt, pub_keys[receipt.generator]))
                     storeReceipt(receipt);
             }
             else{
-                Packet packet = parseMessage(message);
-                processPacket(config, pub_keys, pvt_key, packet, node_id);
+                int prev_node = getNodeID(config, their_addr);
+                Packet packet = parsePacket(packet_str);
+                processPacket(config, pub_keys, packet, pvt_signing, pvt_encryption, node_id, prev_node);
             }
             close(new_fd);
             exit(0);
@@ -318,18 +386,20 @@ unordered_map<int, PubKey> getPubKeys(int node_cnt){
         string path = "keys/pub/" + to_string(i) + ".key";
         ifstream fp(path, ios::binary);
         PubKey curr;
-        fp.read((char*) curr.val, crypto_sign_ed25519_PUBLICKEYBYTES);
+        fp.read((char*) curr.signing, crypto_sign_ed25519_PUBLICKEYBYTES);
+        crypto_sign_ed25519_pk_to_curve25519(curr.encryption, curr.signing);
         pub_keys[i] = curr;
     }
     return pub_keys;
 }
 
-void loadPvtKey(unsigned char* pvt_key){
+void loadPvtKey(unsigned char* pvt_signing, unsigned char* pvt_encryption){
     string path = "keys/pvt.key";
     ifstream fp(path, ios::binary);
     if(!fp.is_open())
         throw runtime_error("unable to load private key");
-    fp.read((char*) pvt_key, crypto_sign_ed25519_SECRETKEYBYTES);
+    fp.read((char*) pvt_signing, crypto_sign_ed25519_SECRETKEYBYTES);
+    crypto_sign_ed25519_sk_to_curve25519(pvt_encryption, pvt_signing);
 }
 
 int main(int argc, char **argv){
@@ -348,19 +418,19 @@ int main(int argc, char **argv){
         int node_cnt = config.size();
         unordered_map<int, PubKey> pub_keys = getPubKeys(node_cnt);
 
-        unsigned char pvt_key[crypto_sign_ed25519_SECRETKEYBYTES];
-        loadPvtKey(pvt_key);
+        unsigned char pvt_signing[crypto_sign_ed25519_SECRETKEYBYTES], pvt_encryption[crypto_box_SECRETKEYBYTES];
+        loadPvtKey(pvt_signing, pvt_encryption);
         
         int sockfd = createServer(config[node_id].port);
         if(!fork()){
             sleep(2);
             close(sockfd);
-            vector<Packet> packets = loadMessages(node_id);
+            vector<Packet> packets = loadMessages(pub_keys, node_id);
             for(Packet packet : packets)
-                processPacket(config, pub_keys, pvt_key, packet, node_id);
+                processPacket(config, pub_keys, packet, pvt_signing, pvt_encryption, node_id, -1);
             exit(0);
         }
-        processConnections(config, pub_keys, pvt_key, sockfd, node_id);
+        processConnections(config, pub_keys, pvt_signing, pvt_encryption, sockfd, node_id);
     }
     catch(exception &e){
         cerr << e.what() << '\n';
